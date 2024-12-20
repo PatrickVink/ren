cab0be7a17b9d7fbe085adb4e12f7ae4d7f5bd25
Patrick Vink <patrickvink@hotmail.com>
2024-12-21 00:49:27 +0100

    use of ConcurrentQueue and SemaphoreSlim. System.CommandLine support

---
+++ Options.cs
@@ -0,0 +1,77 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+internal class Options : RootCommand
+{
+    internal Options() : base("Batch process MP3 files using ffmpeg")
+    {
+        Add(InputArgument);
+        Add(OutputArgument);
+        Add(PoolSizeOption);
+        Add(BitrateOption);
+        Add(CodecOption);
+        Add(PatternOption);
+        Add(FfmpegOption);
+    }
+    internal const int DefaultPoolSize = 8;
+    internal const string DefaultBitrate = "96k";
+    internal const string DefaultCodec = "libmp3lame";
+    internal const string DefaultPattern = "*.mp3";
+    internal const string DefaultFfmpeg = "ffmpeg";
+
+    internal static readonly Argument<string?> InputArgument = new(
+        "input", 
+        "The path to the input folder containing media files");
+    internal static readonly Argument<string?> OutputArgument = new(
+        "output", 
+        () => ".", 
+        "The path to the output folder where processed files will be saved");
+    internal static readonly Option<int> PoolSizeOption = new(
+        ["--poolsize", "-p"], 
+        () => DefaultPoolSize, 
+        "The number of parallel tasks");
+    internal static readonly Option<string?> BitrateOption = new(
+        ["--bitrate", "-b"], 
+        () => DefaultBitrate, 
+        "The bitrate for the output files");
+    internal static readonly Option<string?> CodecOption = new(
+        ["--codec", "-c"], 
+        () => DefaultCodec,
+        "The codec to use for the output files");
+    internal static readonly Option<string?> PatternOption = new(
+        ["--pattern", "-f"],
+        () => DefaultPattern,
+        "The file pattern to search for in the input folder");
+    internal static readonly Option<string?> FfmpegOption = new(
+        ["--ffmpeg", "-e"],
+        () => DefaultFfmpeg,
+        "The location of the ffmpeg executable");
+
+    public string? Input { get; set; }
+    public string? Output { get; set; }
+    public int PoolSize { get; set; } = DefaultPoolSize;
+    public string? Bitrate { get; set; } = DefaultBitrate;
+    public string? Codec { get; set; } = DefaultCodec;
+    public string? Pattern { get; set; } = DefaultPattern;
+    public string? Ffmpeg { get; set; } = DefaultFfmpeg;
+}
+internal static class OptionsExtensions
+{
+    internal static Options Map(this InvocationContext context)
+    {
+        return new()
+        {
+            Input = context.ParseResult.GetValueForArgument<string?>(Options.InputArgument),
+            Output = context.ParseResult.GetValueForArgument<string?>(Options.OutputArgument),
+            PoolSize = context.ParseResult.GetValueForOption<int>(Options.PoolSizeOption),
+            Bitrate = context.ParseResult.GetValueForOption<string?>(Options.BitrateOption),
+            Codec = context.ParseResult.GetValueForOption<string?>(Options.CodecOption),
+            Pattern = context.ParseResult.GetValueForOption<string?>(Options.PatternOption),
+            Ffmpeg = context.ParseResult.GetValueForOption<string?>(Options.FfmpegOption)
+        };
+    }
+    // internal static void SetHandler(this Options options, Func<InvocationContext, Task> handler)
+    // {
+    //     options.SetHandler();
+    // }
+}
--- Program.cs
+++ Program.cs
@@ -1,102 +1,91 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.CommandLine;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-const string _music = @"Z:\Downloads\NewsLazer\Top 4000 Editie 2024\320kbps";
-const string _pattern = "*.mp3";
-const string _bitrate = "96k";
-const string _codec = "libmp3lame";
-
-DownSample(_music, _pattern, _bitrate, _codec);
-
-async void DownSample(string music, string pattern = _pattern, string bitrate = _bitrate, string codec = _codec)
+class Program
 {
-    ProcessPool pool = new(5);
-    const string ffmpeg = @"C:\Apps\ffmpeg\bin\ffmpeg.exe";
-    foreach (var file in Directory.GetFiles(music, pattern))
+    static readonly Options options = [];
+    static async Task Main(string[] args)
     {
-        string path = Path.GetDirectoryName(file) ?? string.Empty;
-        string output = Path.Combine(path, "..", Path.GetFileName(file));
-        string arguments = $"-i \"{file}\" -acodec {codec} -ab {bitrate} \"{output}\"";
 
-        Process? proc = await pool.QueueAsync(ffmpeg, arguments, () => 
+        options.SetHandler(async (context) =>
         {
-            File.SetLastWriteTime(output, File.GetLastWriteTime(file));
+            await ProcessFilesAsync(context.Map());
         });
-        if (proc is null)
-            continue;
 
-        //File.SetLastWriteTime(output, File.GetLastWriteTime(file));
-    }
-}
-void Rename()
-{
-    if (args.Length < 2)
-    {
-        Usage();
-        return;
+        await options.InvokeAsync(args);
     }
 
-    const StringComparison comparisonType = StringComparison.InvariantCultureIgnoreCase;
-    string search = args[0];
-    string replace = args[1];
-    string path = args.Length > 2 ? args[2] : ".";
-    string pattern = args.Length > 3 ? args[3] : "*.*";
-    string[] files = Directory.GetFiles(path, pattern);
-
-    foreach (var file in files)
+    static async Task ProcessFilesAsync(Options options)
     {
-        path = Path.GetDirectoryName(file) ?? string.Empty;
-        string name = Path.GetFileNameWithoutExtension(file);
-        string ext = Path.GetExtension(file);
-        if (name.Contains(search, comparisonType))
+        string[] files = Directory.GetFiles(options.Input!, options.Pattern!, SearchOption.AllDirectories);
+        ConcurrentQueue<Task> tasks = new ConcurrentQueue<Task>();
+
+        using SemaphoreSlim semaphore = new SemaphoreSlim(options.PoolSize);
+
+        foreach (string file in files)
         {
-            string newname = name.Replace(search, replace, comparisonType);
-            string newfile = Path.Combine(path, $"{newname}{ext}");
-            if (File.Exists(newfile))
+            await semaphore.WaitAsync();
+
+            tasks.Enqueue(Task.Run(async () =>
             {
-                Warn("File '{0}' already exists.", newfile);
-                continue;
-            }
+                try
+                {
+                    await ProcessFileAsync(options, file);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    static Task ProcessFileAsync(Options options, string inputFile)
+    {
+        return Task.Run(async () =>
+        {
             try
             {
-                File.Move(file, newfile);
-                if (File.Exists(newfile))
-                    Console.WriteLine($"{name}{ext} => {newname}{ext}");
+                string relativePath = Path.GetRelativePath(options.Input!, inputFile);
+                string outputFile = Path.Combine(options.Output!, relativePath);
+                string? outputFolder = Path.GetDirectoryName(outputFile);
+                if (outputFolder != null) Directory.CreateDirectory(outputFolder);
+                string arguments = $"-hide_banner -loglevel error -y -i \"{inputFile}\" -vn -sn -dn -acodec {options.Codec} -ab {options.Bitrate} \"{outputFile}\"";
 
-            } catch (Exception ex)
-            {
-                Error(ex);
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = options.Ffmpeg,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                using Process? process = Process.Start(startInfo);
+                if (process != null)
+                {
+                    Console.WriteLine($"ffmpeg {arguments}");
+
+                    string output = await process.StandardOutput.ReadToEndAsync(); 
+                    string error = await process.StandardError.ReadToEndAsync(); 
+
+                    process.WaitForExit();
+
+                    await Task.Delay(100); // Delay to ensure the file is fully written
+                    File.SetLastWriteTime(outputFile, File.GetLastWriteTime(inputFile));
+                }   
             }
-        }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine("{0}: {1}", ex.GetType(), ex.Message);
+            }
+        });
     }
-}
-
-void ShowCommandLineArgs()
-{
-    Console.WriteLine(args.Length);
-    foreach (var arg in args)
-        Console.WriteLine(arg);
-}
-
-void Error(Exception ex)
-{
-    var lastBackgroundColor = Console.BackgroundColor;
-    var lastForegroundColor = Console.ForegroundColor;
-    Console.BackgroundColor = ConsoleColor.Red;
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("{0}: {1}", ex.GetType(), ex.Message);
-    Console.BackgroundColor = lastBackgroundColor;
-    Console.ForegroundColor = lastForegroundColor;
-}
-
-void Warn(string format, params object[] args)
-{
-    var lastForegroundColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.DarkYellow;
-    Console.WriteLine(format, args);
-    Console.ForegroundColor = lastForegroundColor;
-}
-
-void Usage()
-{
-    Console.WriteLine("Usage: ren {search} {replace} [{path:.}] [{pattern:*.*}]");
 }
